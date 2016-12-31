eb88462eaf7e3bfbe79686883f073db35eb86a42
tuonglamnguyen <tuonglamn@gmail.com>
2016-12-31 19:30:14 +0700

    fixed

--- src/main/java/com/lam/qltp/webs/WebConfig.java
+++ src/main/java/com/lam/qltp/webs/WebConfig.java
@@ -17,8 +17,8 @@ public class WebConfig extends WebMvcConfigurerAdapter{
 
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/bower/**").addResourceLocations("../assets/vendor/");
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-		registry.addResourceHandler("/bower/**").addResourceLocations("/resources/bower/");
 		registry.addResourceHandler("/css/**").addResourceLocations("/resources/static/css/");
 		registry.addResourceHandler("/js/**").addResourceLocations("/resources/static/js/");
 		registry.addResourceHandler("/img/**").addResourceLocations("/resources/static/img/");
